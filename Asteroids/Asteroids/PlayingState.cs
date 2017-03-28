eafa0dfd494f701e06c511dc1dcd9e01325f11eb
Scott Schepers <schepers.scott@hotmail.com>
2017-03-28 09:59:51 +0200

    Step 2: Completed

--- Asteroids/Asteroids/PlayingState.cs
+++ Asteroids/Asteroids/PlayingState.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Asteroids
 {
     class PlayingState : GameObjectList
     {
+        SpaceShip spaceShip;
         public PlayingState()
         {
             SpriteGameObject background = new SpriteGameObject("spr_background");
+            spaceShip = new SpaceShip();
 
             this.Add(background);
+            this.Add(spaceShip);
         }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            spaceShip.WrapScreen();
+        }
+
     }
 }
---
+++ Asteroids/Asteroids/SpaceShip.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids
+{
+    class SpaceShip : RotatingSpriteGameObject
+    {
+        public SpaceShip()
+            :base("spr_spaceship")
+        {
+            origin = Center;
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            if (inputHelper.IsKeyDown(Keys.Up)) velocity += (AngularDirection * 5);
+            if (inputHelper.IsKeyDown(Keys.Down)) velocity -= (AngularDirection * 5);
+            if (inputHelper.IsKeyDown(Keys.Left)) Degrees += 4;
+            if (inputHelper.IsKeyDown(Keys.Right)) Degrees -= 4;
+        }
+
+        public void WrapScreen()
+        {
+            if (position.X > Asteroids.Screen.X) position.X = 0;
+            if (position.X < 0) position.X = Asteroids.Screen.X;
+            if (position.Y > Asteroids.Screen.Y) position.Y = 0;
+            if (position.Y < 0) position.Y = Asteroids.Screen.Y;
+        }
+    }
+}
