3a85e41d6b1b4306ab209855cff3f7e0937944c2
Scott Schepers <schepers.scott@hotmail.com>
2017-03-28 10:12:23 +0200

    Step 3: Completed

---
+++ Asteroids/Asteroids/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids
+{
+    class Bullet : RotatingSpriteGameObject
+    {
+        Vector2 startPosition;
+        Vector2 startVelocity;
+        public Bullet(Vector2 startPosition, Vector2 startVelocity)
+            :base("spr_Bullet")
+        {
+            this.startPosition = startPosition;
+            this.startVelocity = startVelocity;
+            position = startPosition;
+            AngularDirection = startVelocity;
+            velocity = AngularDirection * 500; 
+        }
+    }
+}
--- Asteroids/Asteroids/PlayingState.cs
+++ Asteroids/Asteroids/PlayingState.cs
@@ -3,25 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace Asteroids
 {
     class PlayingState : GameObjectList
     {
         SpaceShip spaceShip;
+        GameObjectList bullets;
+
         public PlayingState()
         {
             SpriteGameObject background = new SpriteGameObject("spr_background");
             spaceShip = new SpaceShip();
+            bullets = new GameObjectList();
 
             this.Add(background);
             this.Add(spaceShip);
+            this.Add(bullets);
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
             spaceShip.WrapScreen();
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            if (inputHelper.KeyPressed(Keys.Space))
+            {
+                bullets.Add(new Bullet(spaceShip.Position,spaceShip.AngularDirection));
+            }
         }
 
     }
